4f25f957df31eeb6f9375cf39b51ef8abe62f449
deanyang122 <111415664+deanyang122@users.noreply.github.com>
2023-07-26 14:06:00 +0800

    seperate query and embedding

--- embed_query/OpenAiEmbedding.py
+++ embed_query/OpenAiEmbedding.py
@@ -1,4 +1,4 @@
-import json
+import pandas as pd
 from sentence_transformers import SentenceTransformer
 import faiss
 import numpy as np
@@ -6,18 +6,19 @@ import numpy as np
 
 def clean_and_reinput(data_path, model_path, index_path, vectors_path):
     try:
-        # Read data from phone.json
-        with open(data_path, 'r', encoding='utf-8') as file:
-            data = json.load(file)
+        # Read data from Excel file
+        data = pd.read_excel(data_path)
 
         # Initialize SentenceTransformer model
         model = SentenceTransformer(model_path)
 
         # Vectorize the titles
         title_vectors = []
-        for item in data:
-            title_vector = model.encode(item['title'])
+        for _, row in data.iterrows():
+            title_vector = model.encode(row['title'])
             title_vectors.append(title_vector)
+            print("Title:", row['title'])
+            print("Vector:", title_vector)
 
         # Convert title_vectors to a NumPy array
         title_vectors = np.array(title_vectors)
@@ -38,8 +39,8 @@ def clean_and_reinput(data_path, model_path, index_path, vectors_path):
 
 
 # Example usage
-data_path = 'phone.json'
-model_path = 'distiluse-base-multilingual-cased-v2'
+data_path = 'phone2.xlsx'
+model_path = 'paraphrase-multilingual-mpnet-base-v2'
 index_path = 'index.faiss'
 vectors_path = 'title_vectors.npy'
 
--- embed_query/query.py
+++ embed_query/query.py
@@ -1,23 +1,22 @@
-import json
+import pandas as pd
 import streamlit as st
 from sentence_transformers import SentenceTransformer
 import faiss
 import numpy as np
 
-# Read data from phone.json
-with open('embed_query/phone.json', 'r', encoding="utf-8") as file:
-    data = json.load(file)
+# Read data from phone.xlsx
+data = pd.read_excel('embed_query/phone2.xlsx')
 
 # Load the pre-trained SentenceTransformer model
-model = SentenceTransformer('distiluse-base-multilingual-cased-v2')
+model = SentenceTransformer('paraphrase-multilingual-mpnet-base-v2')
 
 # Load the Faiss index and title vectors from files
 index = faiss.read_index('embed_query/index.faiss')
 title_vectors = np.load('embed_query/title_vectors.npy')
 
 # Set up Streamlit app
-st.title("Nearest Neighbor Search")
-query = st.text_input("Enter your query:")
+st.title("大豐智慧分機表")
+query = st.text_input("請輸入您的問題，將為您找到合適的人:")
 if query:
     # Vectorize the query
     query_vector = model.encode(query)
@@ -26,9 +25,16 @@ if query:
     k = 3  # Number of nearest neighbors to retrieve
     distances, indices = index.search(np.array([query_vector]), k)
 
-    # Retrieve the matched content as replies
-    matched_contents = [data[index]['content'] for index in indices[0]]
+    # Retrieve the matched content
+    matched_data = data.iloc[indices[0]]
 
     st.subheader("Matched Contents:")
-    for i, content in enumerate(matched_contents):
-        st.write(f"{i+1}. {content}")
+    for i, row in matched_data.iterrows():
+        st.write(f"{i + 1}. 部門: {row['dept']}")
+        st.write(f"   姓名: {row['name']}")
+        st.write(f"   分機: {row['ext']}")
+        st.write(f"   私人手機: {row['privatePhone']}")
+        st.write(f"   公務手機: {row['publicPhone']}")
+        st.write(f"   手機簡碼65+分機3碼: {row['easyCode']}")
+        st.write(f"   信箱: {row['email']}")
+        st.write("---")
